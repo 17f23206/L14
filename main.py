7a77c79e9e33ee43cf1cd38434871cd053cf87df
Jamie McNally <17f23206@marling.school>
2020-10-21 08:45:36 +0000

    problemo

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-times_table = 5
+times_table = 0
 answer = 0
+print("What times tables so u wish to see")
+times_table = input()
 print(f"Here is the {times_table} times table")
-for x in range(1,11):
+for x in range(1,13):
     answer = x * times_table
     print(f"{x} times {times_table} is {answer}")
